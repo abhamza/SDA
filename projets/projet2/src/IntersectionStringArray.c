afb04883fe23ea7966c1cbfeea327e61373e4c53
Gilles Louppe <g.louppe@gmail.com>
2013-03-21 14:42:38 +0100

    Projet 2

--- projets/projet2/src/IntersectionStringArray.c
+++ projets/projet2/src/IntersectionStringArray.c
@@ -2,7 +2,7 @@
 
 // TODO: You have to implement $compareElements$, $getHashValue$ and
 //       $getIntersection$ defined in Array.h.
-//       - Your implementation of $compareElements$ and $getHashValue$ may
-//       assume that the arguments are of type char*.
+//       - Your implementation of $compareElements$ and $getHashValue$ have to
+//         to specialized to char*.
 //       - HOWEVER, your implementation of $getIntersection$ must be generic
 //       (and relies on $compareElements$ and $getHashValue$).
