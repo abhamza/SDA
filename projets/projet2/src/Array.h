1279bb707b9f4412efa301f8335845af181c3568
Gilles Louppe <g.louppe@gmail.com>
2013-03-21 08:54:31 +0100

    Interface projet 2

--- projets/projet2/src/Array.h
+++ projets/projet2/src/Array.h
@@ -1,15 +1,63 @@
 /* ========================================================================= *
  * Array
+ * An Array is a generic object to store data into a vector.
+ *
+ * An Array is composed of a vector $values$, the length of the vector and
+ * a function pointer $compareFunction$.
+ *
+ * The function pointed by $compareFunction$ compares two elements, let's say
+ * A and B. It returns an integer less than, equal to, or greater than zero
+ * if A is found, respectively, to be less than, to match, or be greater than B.
  * ========================================================================= */
 
 #ifndef _ARRAY_H_
 # define _ARRAY_H_
 
+#include <stdio.h>
+#include <stdbool.h>
 #include <stddef.h>
 
-typedef struct Array_t {
-    void* values;
+/* Structure */
+typedef struct Array_t
+{
+    void** values;
     size_t length;
+    int (*compareFunction)(const void*, const void*);
 } Array;
+
+/* ------------------------------------------------------------------------- *
+ * Create an Array object from the content of the file $fileName$. Each line
+ * of $fileName$ is stored as an element of the Array object.
+ *
+ * The Array object must later be deleted by calling freeArray().
+ *
+ * PARAMETER
+ * fileName         Name of a file
+ *
+ * RETURN
+ * Array            A new pointer to an Array object
+ * NULL             If an error has occured. The function produces an error
+ *                  message on the standard error output.
+ * ------------------------------------------------------------------------- */
+Array* createArrayFromFile(const char* fileName);
+
+/* ------------------------------------------------------------------------- *
+ * Free the allocated memory of the Array object $array$. If the flag
+ * $deleteElements$ is true, the function free() is called on each element.
+ *
+ * PARAMETERS
+ * array            An Array object
+ * deleteElements   Call the function free() on each element contained in
+ *                  $array$
+ * ------------------------------------------------------------------------- */
+void freeArray(Array* array, bool deleteElements);
+
+/* ------------------------------------------------------------------------- *
+ * Print the content of the Array object $array$ on the standard output.
+ *
+ * PARAMETER
+ * array            An Array object
+ * ------------------------------------------------------------------------- */
+void printArray(const Array* array);
 
 #endif // !_ARRAY_H_
--- projets/projet2/src/Intersection.h
+++ projets/projet2/src/Intersection.h
@@ -5,11 +5,26 @@
 #ifndef _INTERSECTION_H_
 # define _INTERSECTION_H_
 
-#include <stddef.h>
 #include <stdbool.h>
-
 #include "Array.h"
 
-Array* intersection(Array* array1, Array* array2);
+/* ------------------------------------------------------------------------- *
+ * Return a new Array object that contains the common elements of two Array
+ * objects. To compare elements, the function use the compareFunction()
+ * provided by the arrays. Therefore, the elements of the two Array must be
+ * compatible in the sense of compareFunction().
+ *
+ * The new Array object must later be deleted by calling
+ * freeArray(array, false). Note that the function copies adresses of common
+ * elements. It doesn't clone the content of common elements.
+ *
+ * PARAMETERS
+ * array1       The first container of elements
+ * array2       The second container of elements
+ *
+ * RETURN
+ * Array        A new Array object that contains the common elements
+ * ------------------------------------------------------------------------- */
+Array* getIntersection(const Array* array1, const Array* array2);
 
 #endif // !_INTERSECTION_H_
