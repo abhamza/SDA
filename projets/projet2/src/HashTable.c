b7332661c2a88a1c6a4fc4f225db41650e5d3609
Julien Becker <J.Becker@ulg.ac.be>
2013-03-21 14:21:57 +0100

    Hello, I've just add two new files :)

---
+++ projets/projet2/src/HashTable.c
@@ -0,0 +1,4 @@
+#include "HashTable.c"
+
+// TODO: You have to implement the opaque structure
+//       and the functions defined in HashTable.h.
---
+++ projets/projet2/src/IntersectionStringArray.c
@@ -0,0 +1,8 @@
+#include "Array.h"
+
+// TODO: You have to implement $compareElements$, $getHashValue$ and
+//       $getIntersection$ defined in Array.h.
+//       Your implementation of $compareElements$ and $getHashValue$ may
+//       assume that theirs arguments are char*.
+//       HOWEVER, your implementation of $getIntersection$ must be generic
+//       (and relies on $compareElements$ and $getHashValue$).
